36fed8aa253fd7a67d2960d0da8a80b862525595
parkj22 <jinyoung.park@vanderbilt.edu>
2023-02-12 15:50:22 -0600

    added tailwindcss

--- vandy-box/src/App.js
+++ vandy-box/src/App.js
@@ -6,17 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-3xl">
           Welcome to Vandy Box!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
