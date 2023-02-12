9cd588544aca85f487d5ea3237e0fc790275bcf9
parkj22 <jinyoung.park@vanderbilt.edu>
2023-02-12 16:20:46 -0600

    added node_modules

---
+++ vandy-box/src/components/Container.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Container = ({title, }) => {
+  return (
+    <div>
+      <span className="">
+        {title}
+      </span>
+      <div></div>
+    </div>
+  )
+}
+
+export default MovieList
