7e5ef73a0df48e8e028af78868c6770e6bceb111
richardtli <43454832+richardtli@users.noreply.github.com>
2023-02-12 16:50:55 -0600

    header done

--- vandy-box/src/App.js
+++ vandy-box/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header'
+import Header from './components/Header'
 
 function App() {
   return(
--- vandy-box/src/components/Header.js
+++ vandy-box/src/components/Header.js
@@ -2,11 +2,13 @@ import React from "react";
 
 function Header() {
     return (
-        <ul>
-            <li>Home</li>
-            <li>My Profile</li>
-            <li>Class</li>
+        <nav className="navbar shadow-md">
+        <ul className="flex bg-amber-200 p-5 gap-10 justify-end pr-10 font-bold">
+            <li className=""><a href="">Home</a></li>
+            <li className=""><a href="">My Profile</a></li>
+            <li className=""><a href="">Trending</a></li>
         </ul>
+        </nav>
     )
 }
 
