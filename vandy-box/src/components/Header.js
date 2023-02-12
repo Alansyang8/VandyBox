5d8585fbffa4ec7521f6602cefc97d5a44c1247a
richardtli <43454832+richardtli@users.noreply.github.com>
2023-02-12 16:57:01 -0600

    added vandybox text

--- vandy-box/src/components/Header.js
+++ vandy-box/src/components/Header.js
@@ -3,7 +3,8 @@ import React from "react";
 function Header() {
     return (
         <nav className="navbar shadow-md">
-        <ul className="flex bg-amber-200 p-5 gap-10 justify-end pr-10 font-bold">
+        <ul className="flex bg-amber-200 p-5 gap-10 justify-end pr-10 font-bold items-center">
+            <li className="mr-auto font text-3xl font-black">VandyBox</li>
             <li className=""><a href="">Home</a></li>
             <li className=""><a href="">My Profile</a></li>
             <li className=""><a href="">Trending</a></li>
