908fcce1885b31ec512cb12e17d76561e99810a9
richardtli <43454832+richardtli@users.noreply.github.com>
2023-02-12 16:32:45 -0600

    first

--- vandy-box/src/App.js
+++ vandy-box/src/App.js
@@ -1,17 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-3xl">
-          Welcome to Vandy Box!
-        </p>
-      </header>
-    </div>
-  );
+  return(
+    <>
+      <Header />
+    </>
+  )
 }
 
 export default App;
---
+++ vandy-box/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return (
+        <ul>
+            <li>Home</li>
+            <li>My Profile</li>
+            <li>Class</li>
+        </ul>
+    )
+}
+
+export default Header
