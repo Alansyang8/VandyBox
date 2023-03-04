755dcd52719066ee86e46c62f62db0ab3637689c
richardtli <43454832+richardtli@users.noreply.github.com>
2023-03-03 18:20:10 -0600

    bolded

--- vandy-box/src/components/Container.js
+++ vandy-box/src/components/Container.js
@@ -4,7 +4,7 @@ const Container = ({containerTitle, children, ...rest}) => {
   return (
     <div className='p-4 m-4 space-y-3 bg-yellow-100 rounded-2xl shadow-xl'>
       <div className='flex'>
-        <span className='text-xl'>{containerTitle}</span>
+        <span className='text-xl font-bold'>{containerTitle}</span>
       </div>
       <div>
         {children}
