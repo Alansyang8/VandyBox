7b461fa91db8b09e0429d59b4ff52bfd3e3bc367
parkj22 <jinyoung.park@vanderbilt.edu>
2023-03-07 00:08:34 -0600

    added modify function

--- vandy-box/src/components/UserProfile.js
+++ vandy-box/src/components/UserProfile.js
@@ -2,6 +2,9 @@ import SuzyBaePic from "../assets/SuzyBaePic.png";
 import UserInfoGrid from "./UserInfoGrid";
 import { useState, useEffect } from "react";
 import MovieSlider from "./MovieSlider";
+import { auth, db } from "../firebase";
+import { modifyAddInfo, modifyName, modifyStatusMsg } from "../api/firebaseWriter";
+import { doc, getDoc } from "firebase/firestore";
 
 const IMG_PATH = "https://image.tmdb.org/t/p/w500";
 const SEARCH_API_URL =
@@ -9,6 +12,10 @@ const SEARCH_API_URL =
 
 const UserProfile = ({ userData }) => {
   const [movieObjects, setMovieObjects] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [statusMsg, setStatusMsg] = useState("");
+  const [additionalInfo, setAdditionalInfo] = useState("");
 
   async function get1Movie(url) {
     const res = await fetch(url);
@@ -48,10 +55,74 @@ const UserProfile = ({ userData }) => {
 
   const [selectedUserInfo, setSelectedUserInfo] = useState("Fav Movies");
 
-  const handleEdit = () => {};
+  const handleEdit = () => {
+    setEditMode(true);
+  };
+
+  const handleUpdate = async () => {
+    const userEmail = auth.currentUser.email;
+    const userIdRef = doc(db, 'userIdMap', userEmail);
+      const docSnap = await getDoc(userIdRef);
+
+      if (docSnap.exists()) {
+        const userId = docSnap.data().userId;
+        modifyName(userId, userName);
+        modifyStatusMsg(userId, statusMsg);
+        modifyAddInfo(userId, additionalInfo);
+      } else {
+        console.error("Could not find document.");
+      }
+  };
 
   return (
     <div>
+      {editMode && (
+        <div className="MovieInfoPopUp absolute inset-0 m-auto w-fit h-fit max-w-screen-xl bg-white  rounded-3xl p-6 flex justify-evenly border-2 flex-col">
+          <div className="flex flex-col space-y-2 mb-8">
+            <label className="font-bold">Name</label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Your name"
+              value={userName}
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
+            />
+            <label className="font-bold">Status Message</label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Status message"
+              value={statusMsg}
+              onChange={(e) => {
+                setStatusMsg(e.target.value);
+              }}
+            />
+            <label className="font-bold">Additional Info</label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Additional info"
+              value={additionalInfo}
+              onChange={(e) => {
+                setAdditionalInfo(e.target.value);
+              }}
+            />
+          </div>
+          <div className="space-x-4">
+            <button
+              className="bg-gray-200 p-2 rounded-md"
+              onClick={() => {
+                setEditMode(false);
+              }}
+            >
+              Cancel
+            </button>
+            <button className="bg-gray-200 p-2 rounded-md text-red-500" onClick={handleUpdate}>
+              Update
+            </button>
+          </div>
+        </div>
+      )}
+
       <div className="flex items-center p-4 w-full">
         <div className="flex  flex-col">
           {/* User Information Section */}
@@ -60,7 +131,8 @@ const UserProfile = ({ userData }) => {
               <img
                 className="h-40 w-40 md rounded-full relative"
                 src={SuzyBaePic}
-                alt=""></img>
+                alt=""
+              ></img>
               {/* <div className="absolute"></div> */}
             </div>
             <div className="flex flex-col space-y-1 justify-center items-left -mt-12 w-80">
@@ -84,7 +156,8 @@ const UserProfile = ({ userData }) => {
                 </div>
                 <div
                   className="my-auto text-white bg-gray-400 hover:bg-gray-500 hover:cursor-pointer rounded-3xl py-2 px-4 mx-2"
-                  onClick={handleEdit}>
+                  onClick={handleEdit}
+                >
                   Edit
                 </div>
                 {/* <div class="my-auto text-gray-800 py-1 px-4 border-2 border-lime-500 hover:bg-lime-500 hover:cursor-pointer hover:text-white rounded-3xl mx-2">Add to Box</div> */}
@@ -108,13 +181,15 @@ const UserProfile = ({ userData }) => {
             className="mr-2 w-1/4 flex justify-center"
             onClick={() => {
               setSelectedUserInfo("Fav Movies");
-            }}>
+            }}
+          >
             <span
               className={
                 selectedUserInfo == "Fav Movies"
                   ? "inline-block p-4 text-lime-600 border-b-2 border-lime-600 rounded-t-lg active dark:text-lime-500 dark:border-lime-500"
                   : "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-              }>
+              }
+            >
               Fav Movies
             </span>
           </div>
@@ -122,13 +197,15 @@ const UserProfile = ({ userData }) => {
             className="mr-2 w-1/4 flex justify-center"
             onClick={() => {
               setSelectedUserInfo("To Watch");
-            }}>
+            }}
+          >
             <span
               className={
                 selectedUserInfo == "To Watch"
                   ? "inline-block p-4 text-lime-600 border-b-2 border-lime-600 rounded-t-lg active dark:text-lime-500 dark:border-lime-500"
                   : "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-              }>
+              }
+            >
               To Watch
             </span>
           </div>
@@ -136,13 +213,15 @@ const UserProfile = ({ userData }) => {
             className="mr-2 w-1/4 flex justify-center"
             onClick={() => {
               setSelectedUserInfo("Friends");
-            }}>
+            }}
+          >
             <span
               className={
                 selectedUserInfo == "Friends"
                   ? "inline-block p-4 text-lime-600 border-b-2 border-lime-600 rounded-t-lg active dark:text-lime-500 dark:border-lime-500"
                   : "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-              }>
+              }
+            >
               Friends
             </span>
           </div>
@@ -150,13 +229,15 @@ const UserProfile = ({ userData }) => {
             className="mr-2 w-1/4 flex justify-center"
             onClick={() => {
               setSelectedUserInfo("Watch Groups");
-            }}>
+            }}
+          >
             <span
               className={
                 selectedUserInfo == "Watch Groups"
                   ? "inline-block p-4 text-lime-600 border-b-2 border-lime-600 rounded-t-lg active dark:text-lime-500 dark:border-lime-500"
                   : "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-              }>
+              }
+            >
               Watch Groups
             </span>
           </div>
