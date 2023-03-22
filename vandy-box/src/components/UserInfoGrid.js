60193230c5cb179cceec37dfbe1cb045ab41ac88
richardtli <43454832+richardtli@users.noreply.github.com>
2023-03-22 18:11:50 -0500

    added recommender and changed firebase movie storage to hold ID

--- vandy-box/src/components/UserInfoGrid.js
+++ vandy-box/src/components/UserInfoGrid.js
@@ -1,39 +1,82 @@
-import React from 'react'
-
+import React from "react";
+import MovieSlider from "./MovieSlider";
+import { useState, useEffect } from "react";
+import SingleMovieFrame from "./SingleMovieFrame";
+const SEARCH_BY_ID_URL_FIRST_HALF = "https://api.themoviedb.org/3/movie/";
+const SEARCH_BY_ID_URL_SECOND_HALF =
+  "?api_key=75e05708188d5f5a0a191495cf4a48db&language=en-US";
+const IMG_PATH = "https://image.tmdb.org/t/p/w500";
 
 function UserInfoGrid({ userData, selectedUserInfo }) {
+  const [favoriteMoviesObjects, setFavoriteMoviesObjects] = useState([]);
+  async function get1MovieByID(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+    return Simplify(data);
+  }
+
+  function Simplify(movie) {
+    const { title, poster_path, overview, release_date, vote_average } = movie;
+
+    return {
+      title: title,
+      image: `${IMG_PATH}${poster_path}`,
+      overview: overview,
+      release_date: release_date,
+      vote_average: vote_average,
+    };
+  }
+
+  async function PopulateFavoriteMovies() {
+    let movieObjectsArray = [];
+    for (const movieID of userData.favorites) {
+      const movieObject = await get1MovieByID(
+        SEARCH_BY_ID_URL_FIRST_HALF + movieID + SEARCH_BY_ID_URL_SECOND_HALF
+      );
+      movieObjectsArray.push(movieObject);
+    }
+    setFavoriteMoviesObjects(movieObjectsArray);
+  }
+
+  useEffect(() => {
+    PopulateFavoriteMovies();
+  }, []);
+
   return (
     <div className="grid grid-cols-4 gap-2">
-        {selectedUserInfo == "Fav Movies" && userData.favorites.map((movie) => (
-            <div className='flex justify-center items-center h-80'>
-                <span className='h-full w-64 flex justify-center items-center border border-yellow-500'>
-                    <span className='text-lg font-bold italic'>{movie}</span>
-                    </span>
+      {(selectedUserInfo == "Fav Movies" && favoriteMoviesObjects.length != 0) && favoriteMoviesObjects.map((movieObject) => (
+          <div className="flex justify-center items-center h-80">
+            <div className="h-full w-64 flex justify-center items-center border border-yellow-500">
+              <SingleMovieFrame movie={movieObject} />
             </div>
+          </div>
         ))}
-        {selectedUserInfo == "To Watch" && userData.toWatch.map((movie) => (
-            <div className='flex justify-center items-center h-80'>
-                <span className='h-full w-64 flex justify-center items-center border border-yellow-500'>
-                    <span className='text-lg font-bold italic'>{movie}</span>
-                    </span>
-            </div>
+      {selectedUserInfo == "To Watch" &&
+        userData.toWatch.map((movie) => (
+          <div className="flex justify-center items-center h-80">
+            <span className="h-full w-64 flex justify-center items-center border border-yellow-500">
+              <span className="text-lg font-bold italic">{movie}</span>
+            </span>
+          </div>
         ))}
-        {selectedUserInfo == "Friends" && userData.friends.map((friend) => (
-            <div className='flex justify-center items-center h-80'>
-                <span className='h-full w-64 flex justify-center items-center border border-yellow-500'>
-                    <span className='text-lg font-bold italic'>{friend}</span>
-                    </span>
-            </div>
+      {selectedUserInfo == "Friends" &&
+        userData.friends.map((friend) => (
+          <div className="flex justify-center items-center h-80">
+            <span className="h-full w-64 flex justify-center items-center border border-yellow-500">
+              <span className="text-lg font-bold italic">{friend}</span>
+            </span>
+          </div>
         ))}
-        {selectedUserInfo == "Watch Groups" && userData.watchGroups.map((group) => (
-            <div className='flex justify-center items-center h-80'>
-                <span className='h-full w-64 flex justify-center items-center border border-yellow-500'>
-                    <span className='text-lg font-bold italic'>{group}</span>
-                    </span>
-            </div>
+      {selectedUserInfo == "Watch Groups" &&
+        userData.watchGroups.map((group) => (
+          <div className="flex justify-center items-center h-80">
+            <span className="h-full w-64 flex justify-center items-center border border-yellow-500">
+              <span className="text-lg font-bold italic">{group}</span>
+            </span>
+          </div>
         ))}
     </div>
-  )
+  );
 }
 
 export default UserInfoGrid;
--- vandy-box/src/components/UserProfile.js
+++ vandy-box/src/components/UserProfile.js
@@ -3,12 +3,33 @@ import UserInfoGrid from "./UserInfoGrid";
 import { useState, useEffect } from "react";
 import MovieSlider from "./MovieSlider";
 import { auth, db } from "../firebase";
-import { modifyAddInfo, modifyName, modifyStatusMsg } from "../api/firebaseWriter";
+import {
+  modifyAddInfo,
+  modifyName,
+  modifyStatusMsg,
+} from "../api/firebaseWriter";
 import { doc, getDoc } from "firebase/firestore";
+import Recommendations from "./Recommendations";
+
 
 const IMG_PATH = "https://image.tmdb.org/t/p/w500";
 const SEARCH_API_URL =
   'https://api.themoviedb.org/3/search/movie?api_key=75e05708188d5f5a0a191495cf4a48db&language=en-US&page=1&include_adult=false&query="';
+
+const SEARCH_BY_ID_URL_FIRST_HALF = "https://api.themoviedb.org/3/movie/";
+const SEARCH_BY_ID_URL_SECOND_HALF =
+  "?api_key=75e05708188d5f5a0a191495cf4a48db&language=en-US";
+let tangled =
+  "https://api.themoviedb.org/3/movie/38757/similar?api_key=75e05708188d5f5a0a191495cf4a48db&language=en-US&page=1";
+
+let GET_SIMILAR_MOVIES_URL_FIRST_THIRD = "https://api.themoviedb.org/3/movie/";
+let GET_SIMILAR_MOVIES_URL_SECOND_THIRD =
+  "/similar?api_key=75e05708188d5f5a0a191495cf4a48db&language=en-US&page=";
+
+let GET_RECOMMENDED_MOVIES_URL_FIRST_THIRD =
+  "https://api.themoviedb.org/3/movie/";
+let GET_RECOMMENDED_MOVIES_URL_SECOND_THIRD =
+  "/recommendations?api_key=75e05708188d5f5a0a191495cf4a48db&language=en-US&page=";
 
 const UserProfile = ({ userData }) => {
   const [movieObjects, setMovieObjects] = useState([]);
@@ -16,16 +37,12 @@ const UserProfile = ({ userData }) => {
   const [userName, setUserName] = useState("");
   const [statusMsg, setStatusMsg] = useState("");
   const [additionalInfo, setAdditionalInfo] = useState("");
+  const[thirtyMovieRec, setThirtyMovieRec] = useState([])
 
-  async function get1Movie(url) {
+  async function get1MovieByID(url) {
     const res = await fetch(url);
     const data = await res.json();
-    let first = getFirst(data.results);
-    return Simplify(first[0]);
-  }
-
-  function getFirst(array) {
-    return array.slice(0, 1);
+    return Simplify(data);
   }
 
   function Simplify(movie) {
@@ -40,17 +57,148 @@ const UserProfile = ({ userData }) => {
     };
   }
 
-  useEffect(() => {
-    async function apiCall() {
-      let movieObjectsArray = [];
-      for (const movieName of userData.topThreeMovies) {
-        const movieObject = await get1Movie(SEARCH_API_URL + movieName);
-        movieObjectsArray.push(movieObject);
-      }
-      setMovieObjects(movieObjectsArray);
+  async function apiCall() {
+    let movieObjectsArray = [];
+    for (const movieID of userData.topThreeMovies) {
+      const movieObject = await get1MovieByID(
+        SEARCH_BY_ID_URL_FIRST_HALF + movieID + SEARCH_BY_ID_URL_SECOND_HALF
+      );
+      movieObjectsArray.push(movieObject);
+    }
+    setMovieObjects(movieObjectsArray);
+  }
+
+  async function getRecommendedMovies() {
+    let movieObjectsArray = [];
+    let totalRecommendedMovies = [];
+    let totalRecommendedMoviesNames = [];
+
+    // for (const movieID of userData.favorites) {
+    //   for (let i = 1; i < 100; i++) {
+    //     totalRecommendedMovies.push(...await getSimilarMovies(GET_RECOMMENDED_MOVIES_URL_FIRST_THIRD + movieID + GET_RECOMMENDED_MOVIES_URL_SECOND_THIRD + i))
+    //   }
+    // }
+
+    // for(const i in totalRecommendedMovies){
+    //   let movieCount = 0
+    //   for(const j in totalRecommendedMovies){
+    //     if(j!= i && totalRecommendedMovies[j].title == totalRecommendedMovies[i].title){
+    //       movieCount++
+    //       if(movieCount>=4){
+    //       movieObjectsArray.push(totalRecommendedMovies[j].title)
+    //       }
+    //     }
+    //   }
+    // }
+
+    for (const movieID of userData.favorites) {
+      totalRecommendedMovies.push(
+        ...(await APIRecommended(
+          GET_RECOMMENDED_MOVIES_URL_FIRST_THIRD +
+            movieID +
+            GET_RECOMMENDED_MOVIES_URL_SECOND_THIRD +
+            1
+        ))
+      );
+      totalRecommendedMovies.push(
+        ...(await APIRecommended(
+          GET_RECOMMENDED_MOVIES_URL_FIRST_THIRD +
+            movieID +
+            GET_RECOMMENDED_MOVIES_URL_SECOND_THIRD +
+            2
+        ))
+      );
     }
 
+    for (let i = userData.favorites.length; i >= 1; i--){
+      totalRecommendedMovies.splice(i * 41 - 21, 1)
+    }
+
+    for (const movieObject of totalRecommendedMovies) {
+      totalRecommendedMoviesNames.push(movieObject.title);
+    }
+
+    // for(const i in totalRecommendedMovies){
+    //   let movieCount = 0
+    //   for(const j in totalRecommendedMovies){
+    //     if(j!= i && totalRecommendedMovies[j].title == totalRecommendedMovies[i].title){
+    //       movieCount++
+    //       if(movieCount>=1){
+    //       movieObjectsArray.push(totalRecommendedMovies[j].title)
+    //       }
+    //     }
+    //   }
+    // }
+
+    var dictMovies = {};
+    var movieCount = 0;
+    let totalRecommendedMoviesNamesSET = new Set(totalRecommendedMoviesNames);
+    for (const movieObjectFromTotal of totalRecommendedMovies) {
+      movieCount = 0;
+      for (const movieName of totalRecommendedMoviesNamesSET) {
+        if (movieObjectFromTotal.title === movieName) {
+          movieCount += 1;
+        }
+      }
+      if (!Object.hasOwn(dictMovies, `${movieObjectFromTotal.title}`)) {
+        dictMovies[`${movieObjectFromTotal.title}`] = [
+          movieCount,
+          movieObjectFromTotal.id
+        ];
+      } else {
+        dictMovies[`${movieObjectFromTotal.title}`] = [
+          dictMovies[`${movieObjectFromTotal.title}`][0] + 1,
+          movieObjectFromTotal.id
+        ];
+      }
+    }
+    console.log(dictMovies);
+    let dictMoviesSorted = []
+
+    for(let i = userData.favorites.length; i >= 1; i--){
+      for(const [key, value] of Object.entries(dictMovies)) {
+        if(value[0] == i){
+          dictMoviesSorted.push(value[1])
+        }
+      }
+    }
+
+    console.log(dictMoviesSorted)
+
+    let best30RecommendedMovieIDS = dictMoviesSorted.slice(0, 30)
+    let best30RecommendedMovieObjects = []
+    for(const MovieID of best30RecommendedMovieIDS) {
+      best30RecommendedMovieObjects.push(await get1MovieByID(SEARCH_BY_ID_URL_FIRST_HALF + MovieID +SEARCH_BY_ID_URL_SECOND_HALF ))
+    }
+
+    setThirtyMovieRec(best30RecommendedMovieObjects)
+
+    console.log(best30RecommendedMovieObjects)
+
+
+ 
+    // for(const movie of totalRecommendedMovies){
+    //   totalRecommendedMoviesNames.push(movie.title)
+    // }
+    // console.log(totalRecommendedMoviesNames.length)
+    // console.log(new Set(totalRecommendedMoviesNames).size)
+  }
+
+  async function getSimilarMovies(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data.results;
+  }
+
+  async function APIRecommended(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data.results;
+  }
+
+  useEffect(() => {
     apiCall();
+    getRecommendedMovies();
   }, []);
 
   const [selectedUserInfo, setSelectedUserInfo] = useState("Fav Movies");
@@ -61,17 +209,17 @@ const UserProfile = ({ userData }) => {
 
   const handleUpdate = async () => {
     const userEmail = auth.currentUser.email;
-    const userIdRef = doc(db, 'userIdMap', userEmail);
-      const docSnap = await getDoc(userIdRef);
+    const userIdRef = doc(db, "userIdMap", userEmail);
+    const docSnap = await getDoc(userIdRef);
 
-      if (docSnap.exists()) {
-        const userId = docSnap.data().userId;
-        modifyName(userId, userName);
-        modifyStatusMsg(userId, statusMsg);
-        modifyAddInfo(userId, additionalInfo);
-      } else {
-        console.error("Could not find document.");
-      }
+    if (docSnap.exists()) {
+      const userId = docSnap.data().userId;
+      modifyName(userId, userName);
+      modifyStatusMsg(userId, statusMsg);
+      modifyAddInfo(userId, additionalInfo);
+    } else {
+      console.error("Could not find document.");
+    }
   };
 
   return (
@@ -112,11 +260,12 @@ const UserProfile = ({ userData }) => {
               className="bg-gray-200 p-2 rounded-md"
               onClick={() => {
                 setEditMode(false);
-              }}
-            >
+              }}>
               Cancel
             </button>
-            <button className="bg-gray-200 p-2 rounded-md text-red-500" onClick={handleUpdate}>
+            <button
+              className="bg-gray-200 p-2 rounded-md text-red-500"
+              onClick={handleUpdate}>
               Update
             </button>
           </div>
@@ -131,34 +280,42 @@ const UserProfile = ({ userData }) => {
               <img
                 className="h-40 w-40 md rounded-full relative"
                 src={SuzyBaePic}
-                alt=""
-              ></img>
+                alt=""></img>
               {/* <div className="absolute"></div> */}
             </div>
             <div className="flex flex-col space-y-1 justify-center items-left -mt-12 w-80">
-              <span data-testid={"name"} className="font-bold text-xl text-center text-gray-800 hover:text-lime-500 hover:cursor-pointer ">
+              <span
+                data-testid={"name"}
+                className="font-bold text-xl text-center text-gray-800 hover:text-lime-500 hover:cursor-pointer ">
                 {userData.name}
               </span>
-              <p data-testid={"handle"} className="text-gray-600 text-sm text-center">
+              <p
+                data-testid={"handle"}
+                className="text-gray-600 text-sm text-center">
                 @{userData.handle}
               </p>
-              <p data-testid={"statusMsg"} className="text-black-600 text-sm text-center">
+              <p
+                data-testid={"statusMsg"}
+                className="text-black-600 text-sm text-center">
                 {userData.statusMsg}
               </p>
-              <p data-testid={"additionalInfo"} className="text-black-600 text-sm text-center">
+              <p
+                data-testid={"additionalInfo"}
+                className="text-black-600 text-sm text-center">
                 {userData.additionalInfo}
               </p>
 
               {/* Follow & Message Buttons */}
               <div className="flex flex-row justify-center font-semibold mx-auto my-4 w-40">
-                <div data-testid="follow" className="my-auto text-white bg-lime-500 hover:bg-lime-600 hover:cursor-pointer rounded-3xl py-2 px-4 mx-2">
+                <div
+                  data-testid="follow"
+                  className="my-auto text-white bg-lime-500 hover:bg-lime-600 hover:cursor-pointer rounded-3xl py-2 px-4 mx-2">
                   Follow
                 </div>
                 <div
                   data-testid="edit"
                   className="my-auto text-white bg-gray-400 hover:bg-gray-500 hover:cursor-pointer rounded-3xl py-2 px-4 mx-2"
-                  onClick={handleEdit}
-                >
+                  onClick={handleEdit}>
                   Edit
                 </div>
                 {/* <div class="my-auto text-gray-800 py-1 px-4 border-2 border-lime-500 hover:bg-lime-500 hover:cursor-pointer hover:text-white rounded-3xl mx-2">Add to Box</div> */}
@@ -182,16 +339,14 @@ const UserProfile = ({ userData }) => {
             className="mr-2 w-1/4 flex justify-center"
             onClick={() => {
               setSelectedUserInfo("Fav Movies");
-            }}
-          >
+            }}>
             <span
               data-testid="favMovies"
               className={
                 selectedUserInfo == "Fav Movies"
                   ? "inline-block p-4 text-lime-600 border-b-2 border-lime-600 rounded-t-lg active dark:text-lime-500 dark:border-lime-500"
                   : "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-              }
-            >
+              }>
               Fav Movies
             </span>
           </div>
@@ -199,16 +354,14 @@ const UserProfile = ({ userData }) => {
             className="mr-2 w-1/4 flex justify-center"
             onClick={() => {
               setSelectedUserInfo("To Watch");
-            }}
-          >
+            }}>
             <span
               data-testid="toWatch"
               className={
                 selectedUserInfo == "To Watch"
                   ? "inline-block p-4 text-lime-600 border-b-2 border-lime-600 rounded-t-lg active dark:text-lime-500 dark:border-lime-500"
                   : "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-              }
-            >
+              }>
               To Watch
             </span>
           </div>
@@ -216,16 +369,14 @@ const UserProfile = ({ userData }) => {
             className="mr-2 w-1/4 flex justify-center"
             onClick={() => {
               setSelectedUserInfo("Friends");
-            }}
-          >
+            }}>
             <span
               data-testid="friends"
               className={
                 selectedUserInfo == "Friends"
                   ? "inline-block p-4 text-lime-600 border-b-2 border-lime-600 rounded-t-lg active dark:text-lime-500 dark:border-lime-500"
                   : "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-              }
-            >
+              }>
               Friends
             </span>
           </div>
@@ -233,21 +384,20 @@ const UserProfile = ({ userData }) => {
             className="mr-2 w-1/4 flex justify-center"
             onClick={() => {
               setSelectedUserInfo("Watch Groups");
-            }}
-          >
+            }}>
             <span
               className={
                 selectedUserInfo == "Watch Groups"
                   ? "inline-block p-4 text-lime-600 border-b-2 border-lime-600 rounded-t-lg active dark:text-lime-500 dark:border-lime-500"
                   : "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-              }
-            >
+              }>
               Watch Groups
             </span>
           </div>
         </div>
       </div>
       <UserInfoGrid userData={userData} selectedUserInfo={selectedUserInfo} />
+     <Recommendations movies={thirtyMovieRec}></Recommendations>
     </div>
   );
 };
